d5c1836e27a7ed0052dc9bc46e287e6fdfe57bb3
renan cruz <renanzinho999@hotmail.com>
2019-10-03 15:22:05 -0300

    Add project files.

---
+++ AppGuardaData/AppGuardaData/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SQLitePCL;
+using PCLExt.FileStorage.Folders;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppGuardaData
+{
+    public partial class App : Application
+    {
+        //conexao com o banco de dados
+        public SQLite.SQLiteConnection conexao { get; private set; }
+        public App()
+        {
+            //Local do banco
+
+            var pasta = new LocalRootFolder();
+
+            //crio o arquivo do banco
+            //se ele existir, abro ele
+
+            var arquivo = pasta.CreateFile("Banco.db", PCLExt.FileStorage.CreationCollisionOption.OpenIfExists);
+
+            //faço a "Conexão"
+
+            conexao = new SQLite.SQLiteConnection(arquivo.Path);
+
+            //criar tabela, se ela não existir
+
+            conexao.Execute("CREATE TABLE IF NOT EXISTS Informacoes (id INTEGER PRIMARY KEY AUTOINCREMENT, info TEXT)");
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ AppGuardaData/AppGuardaData/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppGuardaData
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            InitializeComponent();
+            
+        }
+
+        private void CarregarInformacoes ()
+        {
+
+            string sql = "SELECT * FROM Informacoes";
+            var Lista = ((App)Application.Current).conexao.Query<Model>(sql);
+
+        }
+    }
+}
